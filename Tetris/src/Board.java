2b8102175ec07448436f2863999d0b6c7a99e6d8
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2024-08-10 11:52:55 +0700

    Creating board

---
+++ Tetris/src/Board.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JPanel {
+    private final int BOARD_HEIGHT = 480;
+    private final int BOARD_WIDTH = 300;
+    private final int BLOCK_SIZE = 30;
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        //drawing the board
+        g.setColor(Color.black);
+        g.fillRect(0,0,BOARD_WIDTH, BOARD_HEIGHT);
+
+        g.setColor(Color.white);
+
+        for(int i = 0; i < BOARD_HEIGHT; i++)
+        {
+            g.drawLine(0, i * BLOCK_SIZE, BOARD_HEIGHT, i * BLOCK_SIZE);
+        }
+
+        for (int i = 0; i < BOARD_WIDTH; i++)
+        {
+            g.drawLine(i * BLOCK_SIZE, 0, i * BLOCK_SIZE, BOARD_HEIGHT);
+        }
+    }
+
+}
