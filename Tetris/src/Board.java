71265e2187177612cc9b5549c5587ec2b3f86769
3uPaeJLb <131700005+3uPaeJLb@users.noreply.github.com>
2024-08-24 13:01:08 +0700

    side moving

--- Tetris/src/Board.java
+++ Tetris/src/Board.java
@@ -2,50 +2,54 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Board extends JPanel {
 
-    private final int BOARD_HEIGHT = 510;
-    private final int BOARD_WIDTH = 330;
-    private final int BLOCK_SIZE = 30;
-    private static final int BLOCKS_IN_HEiGHT = 16;
+    private static final int BOARD_HEIGHT = 480;
+    private static final int BOARD_WIDTH = 300;
+    public static final int BLOCK_SIZE = 30;
+    private static final int BLOCKS_IN_HEiGH = 16;
     private static final int BLOCKS_IN_WIDTH = 10;
 
     private int[][] coordinates;
     private Figures figure = new Figures();
-    private int x;
-    private int y;
     private int curX;
     private int curY;
     private Timer timer;
-    private boolean finishedFall = false;
     private Figures.Shapes[] board;
+    private boolean finishedFall = false;
     private int boardTop = 0;
+    private Controller controller;
+
+    public void setCurX(int newX) {
+        curX = newX;
+    }
 
     public Board() {
         figure.setRandomShape();
         curX = 120;
         curY = 0;
-
+        controller = new Controller(this);
         setFocusable(true);
 
-        board = new Figures.Shapes[BLOCKS_IN_WIDTH * BLOCKS_IN_HEiGHT];
-        for (int i = 0; i < BLOCKS_IN_WIDTH * BLOCKS_IN_HEiGHT; i++) {
+        board = new Figures.Shapes[BLOCKS_IN_WIDTH * BLOCKS_IN_HEiGH];
+        for (int i = 0; i < BLOCKS_IN_WIDTH * BLOCKS_IN_HEiGH; i++) {
             board[i] = Figures.Shapes.Noshape;
         }
-        
+
+        addKeyListener(new TAdapter());
+
         timer = new Timer(400, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                if(finishedFall)
-                {
+                if (finishedFall) {
                     finishedFall = false;
-
+                    saveFigure();
                     newFigure();
-                }
-                else
+                } else
                     oneLineDown();
             }
         });
@@ -56,17 +60,13 @@ public class Board extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        //drawing the board
         g.setColor(Color.black);
-        g.fillRect(0,0,BOARD_WIDTH, BOARD_HEIGHT);
-      
-        for(int i = 0; i < BLOCKS_IN_HEiGHT * BLOCKS_IN_WIDTH; i++)
-        {
-            if (board[i] != Figures.Shapes.Noshape)
-            {
-                if (boardTop < BLOCKS_IN_HEiGHT + 1 - (i / BLOCKS_IN_WIDTH) + 1 )
-                {
-                    boardTop = (i / BLOCKS_IN_WIDTH) + 1 - BLOCKS_IN_HEiGHT + 1;
+        g.fillRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
+
+        for (int i = 0; i < BLOCKS_IN_HEiGH * BLOCKS_IN_WIDTH; i++) {
+            if (board[i] != Figures.Shapes.Noshape) {
+                if (boardTop < BLOCKS_IN_HEiGH + 1 - (i / BLOCKS_IN_WIDTH) + 1) {
+                    boardTop = (i / BLOCKS_IN_WIDTH) + 1 - BLOCKS_IN_HEiGH + 1;
                 }
                 int x = i % BLOCKS_IN_WIDTH;
                 int y = i / BLOCKS_IN_WIDTH;
@@ -75,14 +75,13 @@ public class Board extends JPanel {
             }
         }
 
-        if (BLOCKS_IN_HEiGHT - boardTop < 4)
-        {
+        if (BLOCKS_IN_HEiGH - boardTop < 4) {
             timer.stop();
         }
 
         int x = 0;
         int y = 0;
-        
+
         coordinates = figure.getCoords();
 
         for (int i = 0; i < 4; i++) {
@@ -94,7 +93,7 @@ public class Board extends JPanel {
 
         g.setColor(Color.white);
 
-        for(int i = 0; i < BOARD_HEIGHT; i++) {
+        for (int i = 0; i < BOARD_HEIGHT; i++) {
             g.drawLine(0, i * BLOCK_SIZE, BOARD_HEIGHT, i * BLOCK_SIZE);
         }
 
@@ -106,8 +105,7 @@ public class Board extends JPanel {
     private void oneLineDown() {
         if (ableToMove(figure, curX, curY + BLOCK_SIZE)) {
             blockDropped();
-        }
-        else
+        } else
 
             finishedFall = true;
     }
@@ -133,7 +131,16 @@ public class Board extends JPanel {
             timer.stop();
         }
     }
-    
+
+    private void saveFigure() {
+        for (int i = 0; i < 4; i++) {
+            int x = curX + figure.getX(i) * BLOCK_SIZE;
+            int y = curY + figure.getY(i) * BLOCK_SIZE;
+
+            board[((y / BLOCK_SIZE) * BLOCKS_IN_WIDTH) + (x / BLOCK_SIZE)] = figure.getShape();
+        }
+    }
+
     private boolean ableToMove(Figures newFigure, int newX, int newY) {
         for (int i = 0; i < 4; i++) {
             int x = newX + this.figure.getX(i) * BLOCK_SIZE;
@@ -160,4 +167,57 @@ public class Board extends JPanel {
         return board[((y / BLOCK_SIZE) * BLOCKS_IN_WIDTH) + (x / BLOCK_SIZE)];
     }
 
+    class TAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent event) {
+            int code = event.getKeyCode();
+
+            switch (code) {
+                case KeyEvent.VK_LEFT -> {
+                    int check = 0;
+
+                    if (ableToMove(figure, curX, curY)) {
+                        for (int i = 0; i < 4; i++) {
+                            int x = curX + figure.getX(i) * BLOCK_SIZE;
+                            int y = curY + figure.getY(i) * BLOCK_SIZE;
+
+                            check++;
+
+                            if (blockPosition(x - 1, y) != Figures.Shapes.Noshape) {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (check == 4)
+                        controller.moveLeft(curX);
+
+                    check = 0;
+                }
+
+                case KeyEvent.VK_RIGHT -> {
+                    int check1 = 0;
+
+                    if (ableToMove(figure, curX, curY)) {
+                        for (int i = 0; i < 4; i++) {
+                            int x = curX - figure.getX(i);
+                            int y = curY + figure.getY(i);
+
+                            check1++;
+
+                            if (blockPosition(x + 1, y) != Figures.Shapes.Noshape) {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (check1 == 4)
+                        controller.moveRight(curX);
+
+                    check1 = 0;
+                }
+            }
+        }
+    }
 }
+
